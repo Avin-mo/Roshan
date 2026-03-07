48ef3a6e77290411184aad177a9607b742f2f764
linseyqiu-hub <linseyqiu@gmail.com>
2026-03-07 14:14:27 -0800

    about_roshan web set up

---
+++ extension/roshan_site/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import RoshanIntro from './pages/RoshanIntro'
+
+function App() {
+  
+
+  return (
+    <>
+      <RoshanIntro/>
+      
+    </>
+  )
+}
+
+export default App
